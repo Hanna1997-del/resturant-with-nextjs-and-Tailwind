d64baa66fe06cf451ae1aa276f298088c67e33e7
Hanna1997-del <hninthethmue8@gmail.com>
2023-11-07 11:55:35 +0630

    Nav bar and plus with mobile nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import Menu from './Menu'
+import Link from 'next/link'
+import Carticon from './Carticon';
+import Image from 'next/image';
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div>Navbar</div>
+    <div className='h-12 text-red-500 p-4 flex items-center
+     justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40'>
+      {/* left Links */}
+      <div className='hidden md:flex gap-4 flex-1'>
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
+      {/* Logo */}
+      <div className='text-xl md:font-bold flex-1 md:text-center'>
+        <Link href="/">Massimo</Link>
+      </div>
+      {/* mobile Menu */}
+      <div className='md:hidden'>
+        <Menu />
+      </div>
+      {/* Right Links */}
+      <div className='hidden md:flex gap-4 items-center justify-end flex-1'>
+        <div className=' md:absolute top-3 right-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300  rounded-md px-1'>
+          <Image src="/phone.png" alt='' width={20} height={20} />
+          <span>123 456 78</span>
+        </div>
+        {!user ? (<Link href="/login">Login</Link>) :
+        (<Link href="/order">Orders</Link>)}
+        <Carticon />
+      </div>
+    </div>
   )
 }
 
